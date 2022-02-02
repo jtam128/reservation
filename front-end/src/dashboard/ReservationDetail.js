8935db5baf6513808c1bd88febb01c2c7cccae80
jessica tam <jessica.tam128@gmail.com>
2022-02-01 23:35:26 -0500

    Reseration cards file

---
+++ front-end/src/dashboard/ReservationDetail.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { updateReservationStatus } from "../utils/api";
+
+function ReservationDetail({ reservation }) {
+    const history = useHistory();
+
+
+    const [curReservation, setCurReservation] = useState(reservation);
+    useEffect(() => {
+        if (curReservation.status === "booked" || curReservation.status === null)
+        {
+            setShowSeat(true);
+        }
+    }, [curReservation]);
+
+    const handleCancelRes = (e) => {
+        e.preventDefault();
+        setError(null);
+        const confirmBox = window.confirm(
+            "Do you want to cancel this reservation? This cannot be undone."
+        );
+        if (confirmBox === true)
+        {
+            updateReservationStatus({ status: "cancelled" }, curReservation.reservation_id)
+                .then((response) => {
+                    setCurReservation(response);
+                    history.go(0);
+                })
+                .catch(setError);
+        }
+    }
+
+
+    return (
+        <div className="card ">
+            <div className="card-body">
+                <h5 className="card-title">
+                    Reservation for: {`${curReservation.first_name} ${curReservation.last_name}`}{" "}
+                </h5>
+                <p className="card-text">Phone Number: {curReservation.mobile_number}</p>
+                <p className="card-text">Time: {curReservation.reservation_time}</p>
+                <p className="card-text">Party Size: {curReservation.people}</p>
+                <p data-reservation-id-status={curReservation.reservation_id}>
+                    Status:{" "}
+                    <span
+                        className={
+                            curReservation.status === "booked"
+                                ? "card-text text-primary"
+                                : "card-text text-success"
+                        }
+                    >
+                        {curReservation.status}
+                    </span>
+                </p>
+                {curReservation.status === "booked" ? (
+                    <div>
+                        <a
+                            href={`/reservations/${curReservation.reservation_id}/seat`}
+                            className="btn btn-primary mr-1"
+                        >
+                            Seat
+                        </a>
+                        <a
+                            href={`/reservations/${curReservation.reservation_id}/edit`}
+                            className="btn btn-secondary mr-1"
+                        >
+                            {" "}
+                            Edit
+                        </a>
+                        <button
+                            onClick={handleCancelRes}
+                            className="btn btn-danger "
+                            data-reservation-id-cancel={curReservation.reservation_id}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                ) : null}
+            </div>
+        </div>
+    )
+}
+
+export default ReservationDetail;
