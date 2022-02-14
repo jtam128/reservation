b9a16c14cfc036d0668bfcecc311e3918705f18c
jessica tam <jessica.tam128@gmail.com>
2022-02-13 23:41:39 -0500

    add table route

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -5,9 +5,11 @@ const morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
 
+const reservationsRouter = require("./reservations/reservations.router");
+const tablesRouter = require("./tables/tables.router");
+
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
-const reservationsRouter = require("./reservations/reservations.router");
 
 const app = express();
 
@@ -16,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
+app.use("/tables", tablesRouter);
 
 app.use(notFound);
 app.use(errorHandler);
--- front-end/src/dashboard/ReservationDetail.js
+++ front-end/src/dashboard/ReservationDetail.js
@@ -5,25 +5,25 @@ import { updateReservationStatus } from "../utils/api";
 
 function ReservationDetail({ reservation }) {
     const history = useHistory();
-    const [curReservation, setCurReservation] = useState(reservation);
+    const [currentReservation, setCurrentReservation] = useState(reservation);
     const [showSeat, setShowSeat] = useState(false);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        if (curReservation.status === "booked" || curReservation.status === null)
+        if (currentReservation.status === "booked" || currentReservation.status === null)
         {
             setShowSeat(true);
         }
-    }, [curReservation]);
+    }, [currentReservation]);
 
     const handleSeat = (e) => {
         e.preventDefault();
         setError(null);
         setShowSeat(false);
-        updateReservationStatus({ status: "seated" }, curReservation.reservation_id)
+        updateReservationStatus({ status: "seated" }, currentReservation.reservation_id)
             .then((response) => {
-                setCurReservation(response);
-                history.push(`/reservations/${curReservation.reservation_id}/seat`);
+                setCurrentReservation(response);
+                history.push(`/reservations/${currentReservation.reservation_id}/seat`);
             })
             .catch(setError);
     }
@@ -36,48 +36,50 @@ function ReservationDetail({ reservation }) {
         );
         if (confirmBox === true)
         {
-            updateReservationStatus({ status: "cancelled" }, curReservation.reservation_id)
+            updateReservationStatus({ status: "cancelled" }, currentReservation.reservation_id)
                 .then((response) => {
-                    setCurReservation(response);
+                    setCurrentReservation(response);
                     history.go(0);
                 })
                 .catch(setError);
         }
     }
 
-
     return (
         <div className="card ">
             <ErrorAlert error={error} />
+
             <div className="card-body">
                 <h5 className="card-title">
-                    Reservation for: {`${curReservation.first_name} ${curReservation.last_name}`}{" "}
+                    Reservation for: {`${currentReservation.first_name} ${currentReservation.last_name}`}{" "}
                 </h5>
-                <p className="card-text">Phone Number: {curReservation.mobile_number}</p>
-                <p className="card-text">Time: {curReservation.reservation_time}</p>
-                <p className="card-text">Party Size: {curReservation.people}</p>
-                <p data-reservation-id-status={curReservation.reservation_id}>
+                <p className="card-text">Phone Number: {currentReservation.mobile_number}</p>
+
+                <p className="card-text">Time: {currentReservation.reservation_time}</p>
+                <p className="card-text">Party Size: {currentReservation.people}</p>
+
+                <p data-reservation-id-status={currentReservation.reservation_id}>
                     Status:{" "}
                     <span
                         className={
-                            curReservation.status === "booked"
+                            currentReservation.status === "booked"
                                 ? "card-text text-primary"
                                 : "card-text text-success"
                         }
                     >
-                        {curReservation.status}
+                        {currentReservation.status}
                     </span>
                 </p>
-                {curReservation.status === "booked" ? (
+                {currentReservation.status === "booked" ? (
                     <div>
                         <a
-                            href={`/reservations/${curReservation.reservation_id}/seat`}
+                            href={`/reservations/${currentReservation.reservation_id}/seat`}
                             className="btn btn-primary mr-1"
                         >
                             Seat
                         </a>
                         <a
-                            href={`/reservations/${curReservation.reservation_id}/edit`}
+                            href={`/reservations/${currentReservation.reservation_id}/edit`}
                             className="btn btn-secondary mr-1"
                         >
                             {" "}
@@ -86,7 +88,7 @@ function ReservationDetail({ reservation }) {
                         <button
                             onClick={handleCancelRes}
                             className="btn btn-danger "
-                            data-reservation-id-cancel={curReservation.reservation_id}
+                            data-reservation-id-cancel={currentReservation.reservation_id}
                         >
                             Cancel
                         </button>
